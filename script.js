5c3844701cbdcee2deced8567a0005292d0a790b
Javier Nieto <javiale0310@gmail.com>
2026-01-03 22:15:14 -0800

    modified styles.css and index.html
    Style dashboard Section

--- script.js
+++ script.js
@@ -13,4 +13,33 @@ const welcomingSection = document.getElementById("welcoming");
 const dashboardingSection = document.getElementById("dashboarding");
 const favoritingSection = document.getElementById("favoriting");
 
+const welcomingPage = document.getElementById("welcoming-page");
+const dashboardingPage = document.getElementById("dashboarding-page");
+const favoritingPage = document.getElementById("favoriting-page");
 
+dashboardingSection.addEventListener('click', () => {
+    welcomingPage.classList.add("display-hidden");
+    welcomingPage.classList.remove("welcoming-section")
+    favoritingPage.classList.add("display-hidden");
+    favoritingPage.classList.remove("favorites-section");
+    dashboardingPage.classList.add("dashboard-section");
+    dashboardingPage.classList.remove("display-hidden");
+})
+
+favoritingSection.addEventListener('click', () => {
+    welcomingPage.classList.add("display-hidden");
+    welcomingPage.classList.remove("welcoming-section")
+    dashboardingPage.classList.remove("dashboard-section");
+    dashboardingPage.classList.add("display-hidden");
+    favoritingPage.classList.remove("display-hidden");
+    favoritingPage.classList.add("favorites-section");
+})
+
+welcoming.addEventListener('click', () => {
+    dashboardingPage.classList.remove("dashboard-section");
+    dashboardingPage.classList.add("display-hidden");
+    favoritingPage.classList.add("display-hidden");
+    favoritingPage.classList.remove("favorites-section");
+    welcomingPage.classList.remove("display-hidden");
+    welcomingPage.classList.add("welcoming-section")
+})
